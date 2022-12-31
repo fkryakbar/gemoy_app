655d80e0c1ae801a03ecacc852907cc0b7066094
fkrydev <fikri.afa289@gmail.com>
2022-12-31 19:46:59 +0800

    add birthday button

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -15,9 +15,10 @@ export default function About() {
       <Navbar data={1}></Navbar>
       <div className='container m-auto w-[92%] max-w-xl	'>
         <Navigation link="about"></Navigation>
-        <div className='bg-white border-2 rounded-lg py-8 px-2 mt-5'>
+        <div className='bg-white border-2 rounded-lg py-8 px-2 mt-5 text-center'>
           <p className='text-center mb-3'>This App was created with &#10084; and was dedicated to save our memories
           </p>
+          <a href='https://happy21hanna.ninepmx.my.id' className='btn bg-pink-400 border-0 hover:bg-pink-600 mb-3'> Happy Birthday on 21 ! </a>
           <hr />
           <p className='text-center font-bold mt-3'>F & H</p>
         </div>
